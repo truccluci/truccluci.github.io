93064e473df3a370badaf5e5d911cb7d7c181057
truccluci <103984645+truccluci@users.noreply.github.com>
2022-04-24 16:29:40 -0500

    Minor function refactor

--- runs.js
+++ runs.js
@@ -1,15 +1,11 @@
 // 2022-04-19 - luci
-const $ = (id) => {
-    return document.getElementById(id);
-}
-
 // CALCULATE TRUNK CAPACITY
 const calculateCapacity = () => {
-  const trunk = $('trunk').value;
+  const trunk = document.getElementById('trunk').value;
   let multiple = 1.15;
-  if ($('premium').checked == false && $('postop').checked == false) {
+  if (document.getElementById('premium').checked == false && document.getElementById('postop').checked == false) {
     multiple = 1;
-  } else if ($('premium').checked == true && $('postop').checked == true) {
+  } else if (document.getElementById('premium').checked == true && document.getElementById('postop').checked == true) {
     multiple = 1.3;
   }
   let capacity1 = 0;
@@ -28,7 +24,7 @@ const calculateCapacity = () => {
 // CAPACITY TO HTML
 const capacityToHTML = () => {
   const capacities = calculateCapacity();
-  $('result-capacity').insertAdjacentHTML('beforeend', '<span class="temp"><b>' + Math.round(capacities[0] + capacities[1]) + '</b></span>');
+  document.getElementById('result-capacity').insertAdjacentHTML('beforeend', '<p class="temp"><b>' + Math.round(capacities[0] + capacities[1]) + '</b></p>');
 }
 
 // RUNS CALCULATOR
@@ -40,7 +36,7 @@ const runs = (capacity1, capacity2, processWeight, nProcesses) => {
 
 // RUNS RESULTS TO HTML
 const runsToHTML = (result, parent) => {
-  $(parent).insertAdjacentHTML('beforeend', '<p class="temp"><b>' + result + '</b></p>');
+  document.getElementById(parent).insertAdjacentHTML('beforeend', '<p class="temp"><b>' + result + '</b></p>');
 }
 
 // COMPUTE RUNS
@@ -48,11 +44,11 @@ const calculateRuns = () => {
   const capacities = calculateCapacity();
   const capacity1 = capacities[0];
   const capacity2 = capacities[1];
-  const concrete = $('concrete').value;
+  const concrete = document.getElementById('concrete').value;
   const sand = concrete * 25;
 
   // 1. PICKUP: RAW GAS
-  // 2. REFINE: RAW GAS TO WASTE WATER
+  // 2. REFINE: RAW GAS TO WASTE WATER + CHEMICALS
   let result = runs(capacity1, capacity2, 150, (Math.ceil(Math.ceil(sand / 90) * 10 / 5)));
   runsToHTML(result, 'parent-rawgas');
   runsToHTML(result, 'parent-wastewater');
@@ -95,7 +91,7 @@ window.onload = () => {
   calculateRuns();
 }
 
-$('trunk').addEventListener('change', () => {
+document.getElementById('trunk').addEventListener('change', () => {
   document.querySelectorAll('.temp').forEach(e => e.remove());
   capacityToHTML();
   calculateRuns();
@@ -117,7 +113,7 @@ document.querySelectorAll('input[name="postop"]').forEach((elem) => {
   });
 });
 
-$('concrete').addEventListener('change', () => {
+document.getElementById('concrete').addEventListener('change', () => {
   document.querySelectorAll('.temp').forEach(e => e.remove());
   capacityToHTML();
   calculateRuns();
